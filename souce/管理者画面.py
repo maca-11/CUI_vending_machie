d54ec091de491d9ffceff70b94942818c96cb30e
masaki <m.kudou.sys24@morijyobi.ac.jp>
2025-02-17 12:37:57 +0900

    コードの修正１

--- souce/管理者画面.py
+++ souce/管理者画面.py
@@ -16,9 +16,12 @@ def show_admin_panel(products):
     print("商品       単価    販売数    売上金額")
     print("=================================")
     total_sales = 0
+    sales_reset = True  # 売上がリセットされているかどうかを示すフラグ
     for num, item in products.items():
         sales_amount = item["sales"] * item["price"]
         total_sales += sales_amount
+        if item["sales"] != 0:
+            sales_reset = False  # 販売数がリセットされていない場合フラグをFalseに設定
         print(f"{num}. {item['name']}  {item['price']}円   {item['sales']}   {sales_amount}円")
     print("=================================")
     print(f"総売上金額  {total_sales}円")
@@ -33,22 +36,33 @@ def show_admin_panel(products):
             item["sales"] = 0
         print("売上をリセットしました。")
     elif choice == '2':
-        try:
-            print("\n商品番号と新しい価格を入力してください。")
-            product_number = int(input("商品番号: "))
-            new_price = int(input("新しい価格: "))
-            if product_number in products:
-                products[product_number]["price"] = new_price
-                print(f"{products[product_number]['name']}の価格が{new_price}円に変更されました。")
-            else:
-                print("無効な商品番号です。")
-        except ValueError:
-            print("無効な入力です。")
+        if sales_reset:
+            try:
+                print("\n商品番号と新しい価格を入力してください。")
+                product_number = int(input("商品番号: "))
+                new_price = int(input("新しい価格: "))
+                if product_number in products:
+                    products[product_number]["price"] = new_price
+                    print(f"{products[product_number]['name']}の価格が{new_price}円に変更されました。")
+                else:
+                    print("無効な商品番号です。")
+            except ValueError:
+                print("無効な入力です。")
+        else:
+            print("価格を変更する前に売上をリセットしてください。")
+            print("Enterキーを押して続行...")
+            while True:
+                key = keyboard.read_event(suppress=True).name
+                if key == 'enter':
+                    clear_screen()
+                    break
+    elif choice == '3':
+        print("\n管理画面を終了します。")
     
     print("\n管理画面に戻ります...")
-    print("Enterキーを押してください...")
+    print("aキーを押してください...")
     while True:
         key = keyboard.read_event(suppress=True).name
-        if key == 'enter':
+        if key == 'a':
             clear_screen()
             break
